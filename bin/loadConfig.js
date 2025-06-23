ed3bcd9edde567f27019c3265a32e2c948213fde
Marco Zimmermann <fuzzyslash@hotmail.com>
2025-06-22 21:39:38 -0600

    Config error

--- bin/loadConfig.js
+++ bin/loadConfig.js
@@ -2,5 +2,14 @@ import path from 'path';
 
 export default async function loadConfig ()
 {
-    return await import(path.resolve(process.cwd(), 'auto-scope.config.js')).then (mod => mod.default);
+    try {
+        const config = await import(path.resolve(process.cwd(), 'auto-scope.config.js')).then (mod => mod.default);
+    }
+    catch(err)
+    {
+        const e = new Error ("Config not found. Use 'npx install-auto-scope' to generate one.")
+       
+        e.stack = '';
+        throw (e);
+    }
 }
